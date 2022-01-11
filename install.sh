ce44ad9e049de8f2af3e3ddbc441cf122a59e4ff
Eccentrici <96399844+Eccentrici@users.noreply.github.com>
2022-01-11 19:55:31 +1100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,9 @@
 chmod +x esh.py
 mv esh.py esh
 mkdir -p $HOME/.config/esh
-cp esh $HOME/.local/bin/esh
+echo "sudo required to move ESH to '/bin/esh':"
+sudo cp esh /bin/esh
 mv config.toml $HOME/.config/esh/eshconfig.toml
-
+echo "Done."
+echo "You can now delete this repository."
+echo "Run 'esh' in a terminal to start!"
