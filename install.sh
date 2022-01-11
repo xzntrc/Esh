b31bac3b4e06f227d04652816b5db2c4a10356b9
Eccentrici <96399844+Eccentrici@users.noreply.github.com>
2022-01-11 13:09:17 +1100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,6 @@
 chmod +x esh.py
 mv esh.py esh
-mkdir -p ~/bin
-cp esh ~/bin
-mv config.toml ~/bin/eshconfig.toml
-echo 'export PATH=$PATH”:$HOME/bin' >> ~/.zshrc
-echo 'Done!'
-echo 'Your config file is located in ~/bin/eshconfig.toml'
-echo 'If you experienced any install issues, check the install.sh file'
+mkdir -p $HOME/.config/esh
+cp esh $HOME/.local/esh
+mv config.toml $HOME/.config/eshconfig.toml
 
