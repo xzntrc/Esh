d0fac8573980e7f69cff9648da292276d59422a0
Eccentrici <96399844+Eccentrici@users.noreply.github.com>
2022-01-11 13:12:58 +1100

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,5 +2,5 @@ chmod +x esh.py
 mv esh.py esh
 mkdir -p $HOME/.config/esh
 cp esh $HOME/.local/bin/esh
-mv config.toml $HOME/.config/eshconfig.toml
+mv config.toml $HOME/.config/esh/eshconfig.toml
 
